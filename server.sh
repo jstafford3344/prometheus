e7871abcba1eef83267640f00ad9316c3a6cc482
jstafford3344 <jstafford@getsporttrade.com>
2025-04-21 17:24:22 -0400

    chore: permissions

---
+++ server.sh
@@ -0,0 +1,6 @@
+wget https://github.com/prometheus/prometheus/releases/download/v2.53.4/prometheus-2.53.4.linux-amd64.tar.gz
+tar -xvzf prometheus-2.53.4.linux-amd64.tar.gz
+rm prometheus-2.53.4.linux-amd64.tar.gz
+
+cd prometheus-2.53.4.linux-amd64/
+./prometheus
