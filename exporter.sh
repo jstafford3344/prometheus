2b95d2efc1edd0eec1a5e7ecbe8abe7e8af3abfb
jstafford3344 <jstafford@getsporttrade.com>
2025-04-22 20:13:08 -0400

    chore: exporter systemd

---
+++ exporter.sh
@@ -0,0 +1,33 @@
+
+
+SERVICE_PATH="/etc/systemd/system/node_exporter.service"
+
+wget https://github.com/node_exporter/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.linux-amd64.tar.gz
+tar -xvf node_exporter-1.9.1.linux-amd64.tar.gz
+cd node_exporter-1.9.1.linux-amd64/
+
+sudo cp node_exporter /usr/local/bin
+sudo useradd --no-create-home --shell /bin/false node_exporter
+sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
+
+# Set up systemd
+cat > "$SERVICE_PATH" <<EOF
+[Unit]
+Description=node_exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Start node_exporter via systemd
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
+sudo systemctl enable node_exporter
